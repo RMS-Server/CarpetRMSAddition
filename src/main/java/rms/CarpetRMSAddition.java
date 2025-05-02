3fe45a49c2016c5e0e384b2597cae9f51d235d8e
Jog-Ming <jog_ming@outlook.com>
2025-05-02 15:28:45 +0800

    Added version display on /carpet

--- src/main/java/rms/CarpetRMSAddition.java
+++ src/main/java/rms/CarpetRMSAddition.java
@@ -3,15 +3,27 @@ package rms;
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 
 public class CarpetRMSAddition implements CarpetExtension, ModInitializer {
-    @Override
-    public String version() {
-        return "carpet-rms-addition";
+    private static final String ID = "carpet-rms-addition";
+    private static String name;
+    private static String version;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getVersion() {
+        return version;
     }
 
     @Override
     public void onInitialize() {
+        final ModMetadata metadata = FabricLoader.getInstance().getModContainer(ID).orElseThrow(IllegalStateException::new).getMetadata();
+        name = metadata.getName();
+        version = metadata.getVersion().getFriendlyString();
         CarpetServer.manageExtension(new CarpetRMSAddition());
     }
 
---
+++ src/main/java/rms/mixin/SettingsManagerMixin.java
@@ -0,0 +1,19 @@
+package rms.mixin;
+
+import carpet.settings.SettingsManager;
+import carpet.utils.Messenger;
+import carpet.utils.Translations;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rms.CarpetRMSAddition;
+
+@Mixin(SettingsManager.class)
+public class SettingsManagerMixin {
+    @Inject(method = "listAllSettings", at = @At(value = "INVOKE", target = "Lcarpet/settings/SettingsManager;getCategories()Ljava/lang/Iterable;", ordinal = 0), remap = false)
+    private void listAllSettings(ServerCommandSource source, CallbackInfoReturnable<Integer> cir) {
+        Messenger.m(source, "g " + CarpetRMSAddition.getName() + " " + Translations.tr("ui.version", "version") + ": " + CarpetRMSAddition.getVersion());
+    }
+}
